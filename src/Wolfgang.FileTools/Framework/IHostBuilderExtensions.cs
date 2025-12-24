746d5ad4d0f0ea52c3f524da66aa970d511a5860
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-12-23 21:17:43 -0500

    Initial commit of default project

---
+++ src/Wolfgang.FileTools/Framework/IHostBuilderExtensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Wolfgang.FileTools.Framework;
+
+internal enum ConfigurationFileMethod
+{
+    SingleFile,
+    OneFilePerEnvironment
+}
+
+
+
+internal static class IHostBuilderExtensions
+{
+    /// <summary>
+    /// Adds a configuration file to the host builder.
+    /// </summary>
+    /// <param name="builder"> </param>
+    /// <param name="method"> </param>
+    /// <param name="optional" ></param>
+    /// <param name="reloadOnChange"> </param>
+    /// <returns>IHostBuilder</returns>
+    /// <exception cref="ArgumentNullException">builder is null</exception>
+    /// <exception cref="ArgumentOutOfRangeException">method was not a valid value</exception>
+    public static IHostBuilder AddConfigurationFile
+    (
+        this IHostBuilder builder,
+        ConfigurationFileMethod method,
+        bool optional = false,
+        bool reloadOnChange = false
+    )
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        return method switch
+        {
+            ConfigurationFileMethod.SingleFile => AddSingleConfigFile(builder, optional, reloadOnChange),
+            ConfigurationFileMethod.OneFilePerEnvironment => AddConfigFileForEnvironment(builder, optional, reloadOnChange),
+            _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
+        };
+    }
+
+
+
+    private static IHostBuilder AddSingleConfigFile
+    (
+        this IHostBuilder builder,
+        bool optional,
+        bool reloadOnChange
+    )
+    {
+        builder
+            .ConfigureAppConfiguration((context, configurationBuilder) =>
+            {
+                configurationBuilder
+                    .SetBasePath(ConfigurationPath.Combine(AppContext.BaseDirectory))
+                    .AddJsonFile("AppSettings.json", optional, reloadOnChange)
+                    .AddEnvironmentVariables();
+            });
+
+        return builder;
+    }
+
+
+
+    private static IHostBuilder AddConfigFileForEnvironment
+        (
+            this IHostBuilder builder,
+            bool optional,
+            bool reloadOnChange
+        )
+    {
+        var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        if (string.IsNullOrWhiteSpace(environment))
+        {
+            Environment.FailFast("System variable DOTNET_ENVIRONMENT is not set.");
+        }
+
+        builder
+            .ConfigureAppConfiguration((context, configurationBuilder) =>
+            {
+                configurationBuilder
+                    .SetBasePath(ConfigurationPath.Combine(AppContext.BaseDirectory))
+                    .AddJsonFile($"AppSettings.{environment}.json", optional, reloadOnChange)
+                    .AddEnvironmentVariables();
+            });
+        return builder;
+    }
+}
---
+++ src/Wolfgang.FileTools/Program.cs
@@ -0,0 +1,89 @@
+using System.Reflection;
+using JetBrains.Annotations;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Wolfgang.FileTools.Command;
+using Wolfgang.FileTools.Framework;
+
+namespace Wolfgang.FileTools;
+
+[Command
+(
+    Description = "A template for a console application complete with command line parse, logging, DI and more.",
+
+    UnrecognizedArgumentHandling = UnrecognizedArgumentHandling.Throw,
+
+    ResponseFileHandling = ResponseFileHandling.ParseArgsAsLineSeparated
+)]
+
+
+internal class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            // Create a new HostBuilder to build the application
+            return await new HostBuilder()
+                .AddConfigurationFile
+                    (
+                        ConfigurationFileMethod.SingleFile,
+                        optional: false,
+                        reloadOnChange: false
+                    )
+
+                // UseSerilog
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration
+                        .ReadFrom.Configuration(context.Configuration)
+                        .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()?.GetName().Version)
+                        ;
+                })
+
+                // Configure dependency injection
+                .ConfigureServices((_, serviceCollection) =>
+                {
+                    serviceCollection
+                        .AddSingleton<IReporter, ConsoleReporter>()
+                        ;
+                })
+                .RunCommandLineApplicationAsync<Program>(args);
+        }
+        catch (Exception e)
+        {
+            await Console.Error.WriteLineAsync(e.Message);
+            Log.Logger.Fatal(e, "Unhandled exception: {Message}", e.Message);
+            return ExitCode.UnhandledException;
+        }
+        finally
+        {
+            await Log.CloseAndFlushAsync();
+        }
+    }
+
+
+
+    /// <summary>
+    /// This method is called if the user does not specify a sub command
+    /// </summary>
+    /// <param name="application"></param>
+    /// <returns>0 on success or any positive number for failure</returns>
+    /// <remarks>
+    /// - If you are not using sub commands you can rewrite this method to meet your needs
+    /// - You can add and remove any parameters as needed, but you will need to configure dependency injection
+    /// - If you modify this method to do async work, it is recommended to change the signature to
+    ///   Task&lt;int&gt; OnExecuteAsync
+    /// </remarks>
+    [UsedImplicitly]
+    internal int OnExecute
+    (
+        CommandLineApplication<Program> application
+    )
+    {
+        application.ShowHelp();
+        return ExitCode.Success;
+    }
+}
